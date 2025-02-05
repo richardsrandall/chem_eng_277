0128b63cb91961af6970d75b5f6bd382fac66590
richardsrandall <125166277+richardsrandall@users.noreply.github.com>
2025-02-05 11:03:58 -0800

    Got it to successfully read the scale bar on one test image.

--- chem277_process.m
+++ chem277_process.m
@@ -5,21 +5,21 @@ clear
 % 1) Aggs contains all needed data, save to .mat or .csv 
 
 %% Try scale bar first
-manual = imread('Data\346a 2247K 4.5atm\c3_346a_2247K_4.5atm_0016.tif');
-tools.ui_scale_bar(manual)
+manual = imread('data/test_image_1.tif');
+%tools.ui_scale_bar(manual)
+hardcode_scale = 0.8081;
 
 
 %% Load image and attempt to find scale in footer:
-Imgs = tools.load_imgs('Data\346a 2247K 4.5atm');
-imgs = {Imgs.cropped};                              % copy variables locally
+[Imgs, imgs, pixsizes] = tools.load_imgs('data', 1);
+%imgs = {Imgs.cropped};                              % copy variables locally
+
+disp("Pixel sizes found: "+ppixsizes)
 
 %% Try manually adding scale bar
-% Lets us crop scale bar and click on left/right to specify length
-% scale = 0.6157; % nm/px
-scale_346a_0016 = 0.6146; % nm/ox
 
 % Manually assign for now
-Imgs.pixsize = scale_346a_0016;
+%Imgs.pixsizes = {hardcode_scale};
 
 %% Return values
 pixsizes = [Imgs.pixsize];                          % pixel size for each image
@@ -35,6 +35,6 @@ Aggs = agg.analyze_binary(imgs_binary, pixsizes, imgs, fname);
 Aggs = pp.pcm(Aggs); % apply pair correlation method
 
 %% Save the data
-tools.write_excel(Aggs, strcat('processed/5pCH4_2247K/kmeans/process_results.xlsx'));
+tools.write_excel(Aggs, strcat('processed/test_1/kmeans/process_results.xlsx'));
 
-tools.imwrite_agg(Aggs, 'processed/5pCH4_2247K/kmeans')
+tools.imwrite_agg(Aggs, 'processed/test_1/kmeans')
